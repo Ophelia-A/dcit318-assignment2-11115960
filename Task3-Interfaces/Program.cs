df05b5651c49c93f320da8275be60603213147b3
Ophelia Awo Ayisi <151508923+Ophelia-A@users.noreply.github.com>
2025-08-09 01:49:22 -0200

    Task 3 completed

---
+++ Task3-Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task3_Interfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            
+            myCar.Move();       
+            myBicycle.Move();   
+
+            Console.ReadLine();
+        }
+    }
+}
